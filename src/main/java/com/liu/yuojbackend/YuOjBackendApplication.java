e1154666adbd4fc1003f1e8d05c9557c50adbe21
Detail-analy <1580310590@qq.com>
2024-05-16 23:44:33 +0800

    feat:完善用户的crud功能

--- src/main/java/com/liu/yuojbackend/YuOjBackendApplication.java
+++ src/main/java/com/liu/yuojbackend/YuOjBackendApplication.java
@@ -4,6 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
+/**
+ * mapperScan作用
+ * 可以告诉 Spring 在指定的包路径下查找 Mapper 接口，
+ * 并将其注册为 Spring Bean，使得这些 Mapper 接口可以被注入到其他组件中使用
+ */
 @MapperScan("com.liu.yuojbackend.mapper")
 public class YuOjBackendApplication {
 
