2ac64164b0224fcf8320c4229292b8fa81ac0de0
Detail-analy <1580310590@qq.com>
2024-07-21 23:27:12 +0800

    feat:Long类型装json精度丢失配置

---
+++ src/main/java/com/liu/yuojbackend/config/JsonConfig.java
@@ -0,0 +1,29 @@
+package com.liu.yuojbackend.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @Author 刘渠好
+ * @Date 2024-07-21 23:24
+ * spring mvc json 配置
+ */
+@JsonComponent
+public class JsonConfig {
+    /**
+     * 添加 Long 转 json 精度丢失的配置
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
+    }
+}
